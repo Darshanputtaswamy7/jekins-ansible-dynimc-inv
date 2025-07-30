58e00c0d8396c15231630d6bb6d5b71471b1ac7f
Darshanputtaswamy7 <darshanputtaswamy7@gmail.com>
2025-07-30 09:52:30 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -6,7 +6,7 @@ region = "ap-south-1"
 resource "aws_instance"  "AWSInstance" {
  ami = "ami-0b32d400456908bf9"
  instance_type = "t2.micro"
- key_name = "mithuntechnologiesdevopsaws2025"
+ key_name = "Darshanjuly"
  security_groups = ["launch-wizard-2"]
  tags = {
  Name = "terraformserver"
